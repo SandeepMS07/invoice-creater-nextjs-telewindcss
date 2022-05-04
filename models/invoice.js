89ae7ef2a7c6eb945aac0ef29289e154b7dd23cc
SandeepMS07 <sandeepms746@gmail.com>
2022-05-04 15:09:46 +0530

    updated modules:

--- models/invoice.js
+++ models/invoice.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const invoiceSchema = new Schema({
-  invoice_number: {
+  name: {
+    type: String,
+  },
+  email: {
     type: String,
   },
   student_id: {
@@ -11,38 +14,29 @@ const invoiceSchema = new Schema({
   learncab_id: {
     type: String,
   },
-  name: {
+  address: {
     type: String,
   },
-  email: {
+  city: {
     type: String,
   },
-  mobile_no: {
+  state: {
     type: String,
   },
-
-  created_date: {
-    type: Date,
-    default: Date.now,
+  pincode: {
+    type: String,
   },
-
-  invoice_date: {
-    type: Date,
+  country: {
+    type: String,
   },
-  total_amount: {
-    type: Number,
-    default: 0,
-  },
-  amount_paid: {
-    type: Number,
-    default: 0,
-  },
-  taxable_amount: {
-    type: Number,
-    default: 0,
+  gdtNo: {
+    type: String,
   },
   payment_id: {
     type: String,
+  },
+  invoice_date: {
+    type: Date,
   },
 
   items: [
@@ -56,42 +50,17 @@ const invoiceSchema = new Schema({
       amount_paid: {
         type: String,
       },
-      discount: {
-        type: String,
-      },
       plan_code: {
         type: String,
       },
       days: {
         type: Number,
       },
+      discount: {
+        type: String,
+      },
     },
   ],
-
-  SGST: {
-    type: Number,
-    default: 0,
-  },
-  IGST: {
-    type: Number,
-    default: 0,
-  },
-  CGST: {
-    type: Number,
-    default: 0,
-  },
-  city: {
-    type: String,
-  },
-  state: {
-    type: String,
-  },
-  pincode: {
-    type: String,
-  },
-  download_url: {
-    type: String,
-  },
 });
 
 module.exports = mongoose.model("invoice_details", invoiceSchema);
